1cd8631b7f949ee965a3e53f62bcf15fec5bfb53
sasikumar <sasikumar9150476994@gmail.com>
2024-08-01 13:29:46 +0530

    done

--- ANGULAR/MyAngularProject1/src/app/app.module.ts
+++ ANGULAR/MyAngularProject1/src/app/app.module.ts
@@ -5,20 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { CalculatorService } from './calculator.service';
+
+const ROUTES:Routes=[
+{path:'home',component:HomeComponent},
+{path:'contact',component:ContactComponent},
+{path:'about',component:AboutComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    CalculatorService,
   ],
   bootstrap: [AppComponent]
 })
